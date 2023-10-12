311c880e0ff3fc57bd0f5d614cc1311a338c5ee9
danesum <135839788+danesum@users.noreply.github.com>
2023-10-13 00:49:27 +0200

    Add files via upload

---
+++ capstone/frontend/src/components/events.js
@@ -0,0 +1,83 @@
+//import needed elements
+import { React, useState, useEffect } from "react";
+import { Container, Button, Row, Col, Card, Alert } from "react-bootstrap";
+
+function Admin({ user, setUser }) {
+  //define local state
+  const [eventData, setEventData] = useState([]);
+  const [messages, setMessages] = useState("");
+
+  //function to get all data from the event storage
+  //database
+  async function getData() {
+    let key = user.jwt;
+    setMessages("");
+    try {
+      let res = await fetch("/events/get", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer: ${key}`,
+        },
+      });
+      let obj = await res.json();
+      setEventData(obj);
+    } catch (err) {
+      setMessages(err.message);
+      console.log(err.message);
+    }
+  }
+
+  // useeffect to populate data on initial load
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // front end code for logged in section for end users. Displays all events
+  // with nonfunctional button that would forward to where they can buy
+  // tickets
+  return (
+    <Container>
+      {/* hidden container for messages */}
+      {messages.length > 0 && (
+        <Row className="mt-3">
+          <Alert key="primary" variant="primary">
+            {messages}
+          </Alert>
+        </Row>
+      )}
+
+      {/* Main body display showing all upcoming events and their details */}
+      <Col>
+        <Row className="py-3">
+          <h1>Upcoming Events</h1>
+        </Row>
+        <Row>
+          {eventData.map((event) => (
+            <Card className="my-2 mx-auto p-0" style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                style={{ height: "100%" }}
+                src={event.eventImg}
+              />
+              <Card.Body>
+                <Card.Title>{event.eventName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  R {event.price}
+                </Card.Subtitle>
+                <Card.Text>
+                  <strong> Date:</strong> {event.date} <br></br>
+                  <strong>Time:</strong> {event.time}
+                </Card.Text>
+                <Row>
+                  <Button variant="primary">Get Tickets</Button>
+                </Row>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      </Col>
+    </Container>
+  );
+}
+
+export default Admin;
