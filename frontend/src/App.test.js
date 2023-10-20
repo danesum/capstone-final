8506297b1f9524b58304eb569831fd018e8d0cc6
danesum <135839788+danesum@users.noreply.github.com>
2023-10-20 18:22:07 +0200

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("Defaults to Log In page", () => {
-  render(<App />);
-  const h1 = screen.getByText("Log In or Sign Up");
-  expect(h1).toBeInTheDocument();
-});
